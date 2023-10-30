b18e132e07f2a815b074de39d2729182c1b89caa
Brian Aguirre <bpaguirre.93@gmail.com>
2023-10-30 12:33:10 -0400

    remove debug onAppear in RollHistoryRow

--- Rollr/Subviews/Roll History/RollHistoryRow.swift
+++ Rollr/Subviews/Roll History/RollHistoryRow.swift
@@ -79,13 +79,6 @@ struct RollHistoryRow: View {
                             .font(.footnote.bold())
                             .lineLimit(1)
                     }
-                }.onAppear {
-                    print("result: \(die.result)")
-                    print("die id: \(die.objectID)")
-                    guard let lastId = roll.wrappedDice.last?.objectID else { print("No last id"); return }
-                    print("lst id: \(lastId)")
-                    print("is equal: \(die.objectID.isEqual(roll.wrappedDice.last?.objectID))")
-                    print()
                 }
                 
                 // Add a divider unless we're on the last die
